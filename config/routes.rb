c706fa6dc04da1fc3d77f83ccacbce79a3dba103
balatzar <bataprod@gmail.com>
2021-05-23 18:02:05 +0200

    Fix registrations

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,5 @@ Rails.application.routes.draw do
       end
     end
   end
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { registrations: "registrations" }
 end
