77b06b7d4ae6b588fbfad710922018c1c6a25e3d
balatzar <bataprod@gmail.com>
2021-05-25 15:46:23 +0200

    Delete item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -61,7 +61,7 @@ class ItemsController < ApplicationController
   def destroy
     @item.destroy
     respond_to do |format|
-      format.html { redirect_to items_url, notice: "Item was successfully destroyed." }
+      format.html { redirect_to @item.list, notice: "Item was successfully destroyed." }
       format.json { head :no_content }
     end
   end
